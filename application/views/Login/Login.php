cc1e1817c8e4ce36267ab1c9a08d38efe6fc3370
Yonatan Abera <yonatanabera2007@gmail.com>
2020-01-21 16:00:23 +0300

    Login page finished

---
+++ application/views/Login/Login.php
@@ -0,0 +1,28 @@
+
+<div class="hero-wrap js-fullheight" style="background-image: url('<?php echo base_url('assets/images/bg_2.jpg');?>');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-4" >
+                    <div class="h1 align-center" style="color: #fff;"><b style="font-weight: 200;">User login<b></div>
+                    <form action="<?php echo base_url('login/auth');?>">
+
+                        <div class="form-group" >
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" id="username" placeholder="Someone@something.com">
+                        </div>
+                        <div class="form-group" >
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="password">
+                        </div>
+
+                        <div class="form-group" style="text-align: center; ">
+                            
+                            <button class="btn btn-primary btn-lg" style="padding: 12px 85px; " type="submit"><span><b>Login</b></span></button>
+                        </div>
+                    </form>
+                </div>
+            
+        </div>
+      </div>
+    </div>
