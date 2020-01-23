2c7bf82a3d639ed8646753a1ab15f92512f456b8
Nobel <nobelmitiku@gmail.com>
2020-01-23 15:33:27 +0300

    adding the upload resume view

---
+++ application/views/UploadResume/index.php
@@ -0,0 +1,146 @@
+<div class="bg-light" data-stellar-background-ratio="0.5" >
+    <div class="overlay"></div>
+    <div class="container container-padding" >
+        <div class="row">
+            <div class="col-lg-10"><b style="font-size:35px;">Create Resume</b></div>
+            <div class="col-lg-2"><input type="button" value="Upload resume" class="btn btn-secondary" style=";background-color: #51378A"></div>
+        </div>
+        <hr/>
+        <form action="" method="post">
+            <div class="row " style="width: 100%;">
+                <div class=" col-lg-3">
+                    <b class="main-title">Contact information</b>
+                </div>
+                <div class="col-lg-4">
+                        <div class="form-group">
+                            <label for="FirstName"style="color:#000;">First Name</label>
+                            <input class="form-control" type="text" name="FirstName" id="FirstName">
+                        </div>
+                        <div class="form-group">
+                            <label for="LastName"style="color:#000;">LastName</label>
+                            <input class="form-control" type="text" name="LastName" id="LastName">
+                        </div>
+                        <div class="form-group">
+                            <label for="Location"style="color:#000;">Location</label>
+                            <input class="form-control" class="form-control" type="Location" name="Location" id="Location">
+                        </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4">
+                    <div class="form-group">
+                        <label for="Email"style="color:#000;">Email</label>
+                        <input class="form-control" class="form-control" type="email" name="Email" id="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="PhoneNumber"style="color:#000;">Phone Number</label>
+                        <input class="form-control" class="form-control" type="tel" name="PhoneNumber" id="PhoneNumber">
+                    </div>
+                </div>
+
+                <div class=" col-lg-3">
+                    <b class="main-title">Profetion</b>
+                </div>
+                <div class="col-lg-4">
+                    <hr/>
+                    <div class="form-group">
+                        <label for="ProfetionalTitle"style="color:#000;">Profetional Title</label>
+                        <input class="form-control" class="form-control" type="text" name="ProfetionalTitle" id="ProfetionalTitle">
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4"><hr/></div>
+                <div class=" col-lg-3">
+                    <b class="main-title">Experiance</b>
+                </div>
+                <div class="col-lg-4">
+                    <hr/>
+                    <div class="form-group">
+                        <label for="Job-title"style="color:#000;">Job Title</label>
+                        <input class="form-control" type="text" name="job-title" id="Job-title">
+                    </div>
+                    <div class="form-group">
+                        <label for="StartDate"style="color:#000;">Start Date</label>
+                        <input class="form-control" class="form-control" type="date" name="StartDate" id="StartDate">
+                    </div>
+                    <div class="form-group">
+                        <label for="JobLocation"style="color:#000;">Job Location</label>
+                        <input class="form-control" class="form-control" type="text" name="JobLocation" id="JobLocation">
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4">
+                    <hr/>
+                    <div class="form-group">
+                        <label for="CompanyName"style="color:#000;">Company Name</label>
+                        <input class="form-control" type="text" name="CompanyName" id="CompanyName">
+                    </div>
+                    <div class="form-group">
+                        <label for="EndDate"style="color:#000;">End Date</label>
+                        <input class="form-control" class="form-control" type="date" name="EndDate" id="EndDate">
+                    </div>
+                </div>
+                <hr/>
+                <div class=" col-lg-3">
+                    <b class="main-title">Education</b>
+                </div>
+                <div class="col-lg-4">
+                    <hr/>
+                    <div class="form-group">
+                        <label for="NameOfInstitution"style="color:#000;">Name Of Institution</label>
+                        <input class="form-control" type="text" name="NameOfInstitution" id="NameOfInstitution">
+                    </div>
+                    <div class="form-group">
+                        <label for="EducationalLevel"style="color:#000;">Educational Level</label>
+                        <input class="form-control" class="form-control" type="text" name="EducationalLevel" id="EducationalLevel">
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4">
+                    <hr/>
+                    <div class="form-group">
+                        <label for="SchoolLocation"style="color:#000;">Where You Studied</label>
+                        <input class="form-control" class="form-control" type="text" name="SchoolLocation" id="SchoolLocation">
+                    </div>
+                    <div class="form-group">
+                        <label for="YearsAttended"style="color:#000;">Years Attended</label>
+                        <input class="form-control" class="form-control" type="number" name="YearsAttended" id="YearsAttended">
+                    </div>
+                </div>
+                <hr/>
+                <div class=" col-lg-3">
+                    <b class="main-title">Skills</b>
+                </div>
+                <div class="col-lg-9">
+                    <hr/>
+                    <div class="form-group">
+                        <textarea rows="3" class="form-control" type="text" name="Skills" id="Skills"></textarea>
+                    </div>
+                </div>
+                <div class=" col-lg-3">
+                    <b class="main-title">Additional Information</b>
+                </div>
+                <hr/>
+                <div class="col-lg-9">
+                    <hr/>
+                    <div class="form-group">
+                        <textarea rows="3" class="form-control" type="text" name="AdditionalInformation" id="AdditionalInformation"></textarea>
+                    </div>
+                </div>
+                <div class=" col-lg-3">
+                    <b class="main-title">Summery</b>
+                </div>
+                <hr/>
+                <div class="col-lg-9">
+                    <hr/>
+                    <div class="form-group">
+                        <textarea class="form-control" type="text" rows="3" name="Summery" id="Summery"></textarea>
+                    </div>
+                </div>
+                <div class=" col-lg-3"></div>
+                <div class="col-lg-9">
+                    <input  class="btn btn-success" type="submit" value="Submit Resume"style="margin-bottom: 50px">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
