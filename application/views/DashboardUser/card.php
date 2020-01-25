eab679d9a53dc95317458e2471df4853800939eb
Yonatan Abera <yonatanabera2007@gmail.com>
2020-01-25 07:37:31 +0300

    Changed the gradient degree to 340 deg

--- application/views/DashboardUser/card.php
+++ application/views/DashboardUser/card.php
@@ -14,7 +14,7 @@
                     
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                        <div class="card h-100 bg-secondary text-white card_menu" style="background: linear-gradient(20deg, #6c757d 0, #5e72e4 100%) !important; " >
+                        <div class="card h-100 bg-secondary text-white card_menu" style="background: linear-gradient(340deg, #6c757d 0, #5e72e4 100%) !important; " >
                             <div class="card-body my-auto" >
                             <i class="icon-bullhorn " style="font-size: 48px; color: #fff;"></i>
                                <h4 class="inner" style="font-size: 16px;">Upload Vacancy</h4>
@@ -26,7 +26,7 @@
 
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                            <div class="card h-100 bg-success text-white card_menu" style="background: linear-gradient(20deg, #28a745  0, #7a5649 100%) !important; ">
+                            <div class="card h-100 bg-success text-white card_menu" style="background: linear-gradient(340deg, #28a745  0, #2efaed 100%) !important; ">
                                 <div class="card-body my-auto">
                                 <i class="icon-file " style="font-size: 48px; color: #fff;"></i>
                                    <h4 class="inner" style="font-size: 16px;">Company Profile</h4>
@@ -37,7 +37,7 @@
 
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                            <div class="card h-100 bg-warning text-white card_menu" style="background: linear-gradient(20deg, #ffc101 0, #923020 100%) !important; ">
+                            <div class="card h-100 bg-warning text-white card_menu" style="background: linear-gradient(340deg, #ffc101 0, #88e212 100%) !important; ">
                                 <div class="card-body my-auto">
                                 <i class="icon-spinner " style="font-size: 48px; color: #fff;"></i>
                                      <h4 class="inner" style="font-size: 16px;">Search Candidate</h4>
@@ -48,7 +48,7 @@
 
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                            <div class="card h-100 bg-transparent text-primary card_menu" style="background: linear-gradient(20deg, #5e72e4 0, #133020 100%) !important; ">
+                            <div class="card h-100 bg-transparent text-primary card_menu" style="background: linear-gradient(340deg, #5e72e4 0, #133020 100%) !important; ">
                                 <div class="card-body my-auto bg-transparent">
                                 <i class="icon-user " style="font-size: 48px; color: #fff;"></i>
                                    <h4 class="inner" style="color: #fff; font-size: 16px;">View Profile</h4>
@@ -61,7 +61,7 @@
                      
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                        <div class="card h-100 bg-info text-white card_menu" style="background: linear-gradient(20deg, #17a2b8  0, #5e72e4 100%) !important; ">
+                        <div class="card h-100 bg-info text-white card_menu" style="background: linear-gradient(340deg, #17a2b8  0, #5e72e4 100%) !important; ">
                             <div class="card-body my-auto" >
                             <i class="icon-hourglass " style="font-size: 48px; color: #fff;"></i>
                                <h4 class="inner">Remaining Balance</h4>
@@ -73,10 +73,10 @@
 
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                            <div class="card h-100 bg-muted text-white card_menu" style="background: linear-gradient(20deg, #f4f8fb   0, #c8c8c8 100%) !important; ">
+                            <div class="card h-100 text-white card_menu" style="background: linear-gradient(340deg, #20c997   0, #135895 100%) !important; ">
                                 <div class="card-body my-auto">
-                                <i class="icon-podcast " style="font-size: 48px; color: #912151;"></i>
-                                   <h4 class="inner" style="font-size: 16px; color: #912151;">Advertisement</h4>
+                                <i class="icon-podcast " style="font-size: 48px; color: #fff;"></i>
+                                   <h4 class="inner" style="font-size: 16px; color: #fff;">Advertisement</h4>
                                 </div>
                             </div>
                         </a>
@@ -84,7 +84,7 @@
 
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                            <div class="card h-100 bg-dark text-white card_menu" style="background: linear-gradient(20deg, #343a40 0, #c8c8c8 100%) !important; ">
+                            <div class="card h-100 bg-dark text-white card_menu" style="background: linear-gradient(340deg, #343a40 0, #c8c8c8 100%) !important; ">
                                 <div class="card-body my-auto">
                                 <i class="icon-search " style="font-size: 48px; color: #fff;"></i>
                                      <h4 class="inner" style="font-size: 16px;">Searh Job</h4>
@@ -95,7 +95,7 @@
 
                     <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 mb-3 jumps" style="height: 100px; ">
                         <a href="#">
-                            <div class="card h-100 bg-danger text-primary card_menu" style="background: linear-gradient(20deg, #dc3545  0, #dfea4d 100%) !important; ">
+                            <div class="card h-100 bg-danger text-primary card_menu" style="background: linear-gradient(340deg, #dc3545  0, #dfea4d 100%) !important; ">
                                 <div class="card-body my-auto bg-transparent">
                                 <i class="icon-file-text " style="font-size: 48px; color: #fff;;"></i>
                                    <h4 class="inner" style="color: #fff; font-size: 16px;">Resume</h4>
